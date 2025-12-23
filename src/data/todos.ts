bdbf65680312711096308c924ba39a407158ca6b
dhaniverdant <dhaniverdant23@gmail.com>
2025-12-24 05:17:04 +0800

    create types and dummy data

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Like this",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Subscribe this",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Comment this",
+    completed: false,
+  },
+]
