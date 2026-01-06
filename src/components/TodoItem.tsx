349da27b31d8b87097345dbc73e75ac898b74a71
dhaniverdant <dhaniverdant23@gmail.com>
2026-01-07 05:51:05 +0800

    add onCompletedChange interface in todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,12 +2,18 @@ import type { Todo } from "../types/todo";
 
 interface TodoItemProps {
   todo: Todo;
+  onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function TodoItem({ todo }: TodoItemProps) {
+export default function TodoItem({ todo, onCompletedChange }: TodoItemProps) {
   return (
     <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 hover:bg-gray-200 cursor-pointer">
-      <input type="checkbox" className="scale-125" />
+      <input
+        type="checkbox"
+        className="scale-125"
+        checked={todo.completed}
+        onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
+      />
       <span className={todo.completed ? "line-through text-gray-400" : ""}>
         {todo.title}
       </span>
